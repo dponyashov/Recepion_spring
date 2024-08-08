7dec7fd28366b3af526c51fa62c8474867650471
dponyashov <ponyashov@gmail.com>
2024-08-08 20:17:53 +0300

    add online record service with telebot

--- reception-online-record/src/main/java/ru/dponyashov/controller/BotController.java
+++ reception-online-record/src/main/java/ru/dponyashov/controller/BotController.java
@@ -1,7 +1,5 @@
 package ru.dponyashov.controller;
 
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
